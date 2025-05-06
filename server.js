90e9f0119db6c1848c502b9e83a05ebdbbfeaca4
ajithks32 <ajithks5532@gmail.com>
2025-05-06 19:43:36 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+app.use(cors());
+// Routes
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+  const uploadDir = path.join(__dirname, "uploads");
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);}
+
+  
+
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/admin", require("./routes/adminRoutes"));
+app.use("/bikes", require("./routes/bikeRoutes"));
+app.use("/bookings", require("./routes/bookingRoutes"));
+app.use("/rental", require("./routes/rentalcarbooking"));
+app.use("/offers",require("./routes/offerRoutes"));
+app.use("/taxis", require("./routes/taxiRoutes"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/analytics", require("./routes/analyticsRoutes"));
+app.use("/carsbanner", require("./routes/carsbannerRoutes"));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
